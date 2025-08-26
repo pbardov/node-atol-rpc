d05ddaf441318d8dc63327e41003dcb1a43b2ae2
Pavel Bardov <pbardov@gmail.com>
2025-08-26 14:16:20 +0500

    Расширен набор типов дат и времени Атол

--- src/types/atol-date.ts
+++ src/types/atol-date.ts
@@ -1,4 +1,15 @@
-import {makeIsYearMonthDay, type YearMonthDay} from '../common/types/year-month-day.js';
+import { makeIsYearMonthDay, type YearMonthDay } from '../common/types/year-month-day.js';
+import structureValidator from '../common/types/structure-validator.js';
+import isString from '../common/types/is-string.js';
+import { TypeGuard } from '../common/types/type-guard.js';
 
+// Define AtolDate for yyyy.mm.dd format
 export type AtolDate = YearMonthDay<'.'>;
 export const isAtolDate = makeIsYearMonthDay('.');
+
+// Define AtolDateTime for yyyy.mm.dd HH:MM:SS format
+export type AtolDateTime = `${AtolDate} ${string}`;
+
+// TypeGuard for AtolDateTime
+export const isAtolDateTime: TypeGuard<AtolDateTime> = (v: unknown): v is AtolDateTime =>
+  isString(v) && /^\d{4}\.\d{2}\.\d{2} \d{2}:\d{2}:\d{2}$/.test(v as string);
