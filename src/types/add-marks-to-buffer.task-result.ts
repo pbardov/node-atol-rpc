7f5d8a07ace07478c4cb1ce52ec5878ab09cee9b
Pavel Bardov <pbardov@gmail.com>
2025-10-02 12:39:31 +0500

    Исправление типа AddMarksToBufferTaskResult

--- src/types/add-marks-to-buffer.task-result.ts
+++ src/types/add-marks-to-buffer.task-result.ts
@@ -5,15 +5,19 @@ import {
 } from './get-marking-code-validation-status.task-result.js';
 import {isOfflineValidationResult, type OfflineValidationResult} from './begin-marking-code-validation.task-result.js';
 import structureValidator from '../common/types/structure-validator.js';
+import {arrayTypeGuard} from '../common/types/is-array-of.js';
 
-export type AddMarksToBufferTaskResult = {
+export type AddMarkToBufferResult = {
     driverError: DriverError;
     offlineValidation: OfflineValidationResult;
     itemInfoCheckResult: ImcCheckResult;
 };
 
-export const isAddMarksToBufferTaskResult = structureValidator<AddMarksToBufferTaskResult>({
+export const isAddMarkToBufferResult = structureValidator<AddMarkToBufferResult>({
     driverError: isDriverError,
     offlineValidation: isOfflineValidationResult,
     itemInfoCheckResult: isImcCheckResult,
 });
+
+export type AddMarksToBufferTaskResult = AddMarkToBufferResult[];
+export const isAddMarksToBufferTaskResult = arrayTypeGuard(isAddMarkToBufferResult);
