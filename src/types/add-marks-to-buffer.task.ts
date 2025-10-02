34899dcaef0acb6dbac9159c305ec055e0019644
Pavel Bardov <pbardov@gmail.com>
2025-10-02 12:42:41 +0500

    Незначительные исправления

--- src/types/add-marks-to-buffer.task.ts
+++ src/types/add-marks-to-buffer.task.ts
@@ -3,8 +3,9 @@ import {type ImcParamsValidation, isImcParamsValidation} from './imc-params.js';
 import structureValidator from '../common/types/structure-validator.js';
 import isEqual from '../common/types/is-equal.js';
 import {arrayTypeGuard} from '../common/types/is-array-of.js';
+import type {JsonTask} from './json-task.js';
 
-export type AddMarksToBufferTask = {
+export type AddMarksToBufferTask = JsonTask & {
     type: JsonTaskType.addMarksToBuffer;
     params: ImcParamsValidation[];
 };
