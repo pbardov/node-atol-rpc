a80cf395a7fb532746f9d284af6c4509e07cadf5
Pavel Bardov <pbardov@gmail.com>
2025-10-02 11:53:32 +0500

    Уточнен тип beginMarkingCodeValidation

--- src/types/begin-marking-code-validation.task.ts
+++ src/types/begin-marking-code-validation.task.ts
@@ -1,15 +1,28 @@
 import {JsonTaskType} from './json-task-type.js';
-import {type ImcParamsValidation, isImcParamsValidation} from './imc-params.js';
+import {type ImcParamsValidation, imcParamsValidationValidator, isImcParamsValidation} from './imc-params.js';
 import isEqual from '../common/types/is-equal.js';
 import structureValidator from '../common/types/structure-validator.js';
 import {arrayTypeGuard} from '../common/types/is-array-of.js';
+import {makeOpt} from '../common/types/is-opt.js';
+import isBoolean from '../common/types/is-boolean.js';
+
+export type MarkingCodeValidationParams = ImcParamsValidation & {
+    notSendToServer?: boolean;
+    notFormRequest?: boolean;
+};
+
+export const isMarkingCodeValidationParams = structureValidator<MarkingCodeValidationParams>({
+    ...imcParamsValidationValidator,
+    notSendToServer: makeOpt(isBoolean),
+    notFormRequest: makeOpt(isBoolean),
+});
 
 export type BeginMarkingCodeValidationTask = {
     type: JsonTaskType.beginMarkingCodeValidation;
-    params: ImcParamsValidation[];
+    params: MarkingCodeValidationParams;
 };
 
 export const isBeginMarkingCodeValidationTask = structureValidator<BeginMarkingCodeValidationTask>({
     type: isEqual(JsonTaskType.beginMarkingCodeValidation),
-    params: arrayTypeGuard(isImcParamsValidation),
+    params: isMarkingCodeValidationParams,
 });
