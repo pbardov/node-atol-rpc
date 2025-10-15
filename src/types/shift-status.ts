f1c9df1b328c32a48b68839a3889f0d15c617291
Pavel Bardov <pbardov@gmail.com>
2025-10-15 10:17:08 +0500

    ShiftStatus.documentCount сделано опциональный, ККТ не всегда возвращает его

--- src/types/shift-status.ts
+++ src/types/shift-status.ts
@@ -5,14 +5,14 @@ import isString from '../common/types/is-string.js';
 import {isShiftState, type ShiftState} from './shift-state.js';
 
 export type ShiftStatusInfo = {
-	documentsCount: number;
+	documentsCount?: number;
 	expiredTime?: string;
 	number: number;
 	state: ShiftState;
 };
 
 export const isShiftStatusInfo = structureValidator<ShiftStatusInfo>({
-	documentsCount: isNumber,
+	documentsCount: makeOpt(isNumber),
 	expiredTime: makeOpt(isString),
 	number: isNumber,
 	state: isShiftState,
