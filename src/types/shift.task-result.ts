8e5909c5d897d713e39a059293f6ae646ca2e09e
Pavel Bardov <pbardov@gmail.com>
2025-10-16 23:10:32 +0500

    Исправление в определении ShiftTaskResult

--- src/types/shift.task-result.ts
+++ src/types/shift.task-result.ts
@@ -1,32 +1,18 @@
-import {isIsoDateString, type IsoDateString} from '../common/types/iso-date-string.js';
-import isNumber from '../common/types/is-number.js';
-import isString from '../common/types/is-string.js';
 import {makeOpt} from '../common/types/is-opt.js';
 import structureValidator from '../common/types/structure-validator.js';
 import {type TypeGuardDetail} from '../common/types/type-guard.js';
 import {isResultWarnings, type ResultWarnings} from './result-warnings.js';
+import {type FiscalParams, isFiscalParams} from './fiscal-params.js';
 
 export type ShiftTaskResult = {
-	fiscalDocumentNumber: number;
-	fiscalDocumentSign: string;
-	fiscalDocumentDateTime: IsoDateString;
-	shiftNumber: number;
-	fnNumber: string;
-	registrationNumber: string;
-	fnsUrl: string;
+	fiscalParams: FiscalParams;
 	warnings?: ResultWarnings;
 };
 export type OpenShiftTaskResult = ShiftTaskResult;
 export type CloseShiftTaskResult = ShiftTaskResult;
 
 export const isShiftTaskResult = structureValidator<ShiftTaskResult>({
-	fiscalDocumentNumber: isNumber,
-	fiscalDocumentSign: isString,
-	fiscalDocumentDateTime: isIsoDateString,
-	shiftNumber: isNumber,
-	fnNumber: isString,
-	registrationNumber: isString,
-	fnsUrl: isString,
+	fiscalParams: isFiscalParams,
 	warnings: makeOpt(isResultWarnings),
 });
 export const isOpenShiftTaskResult: TypeGuardDetail<OpenShiftTaskResult> = isShiftTaskResult;
